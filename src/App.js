03bff517e7fc59e7cbd18c1798cb65b769ef89df
[Brandon Teague] <[teaguebrandon@outlook.com]>
2020-08-31 17:11:58 -0400

    basic setup and dependencies installed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+
+function App(props) {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default App;
